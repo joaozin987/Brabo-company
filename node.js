31c2d4e10e473649581d1c5d78f2a97b55cbb421
Júlio César <juliocesar@MacBook-Air-de-Julio.local>
2024-06-03 18:42:20 -0300

    atual

--- node.js
+++
@@ -1,7 +0,0 @@
-const meuBotao = document.getElementById("meuBotao");
-const mensagem = document.getElementById("mensagem");
-
-meuBotao.addEventListener("click", function () {
-  mensagem.style.display = "block";
-  mensagem.innerHTML = "Olá voce clicou no botao.";
-});
